d830628b4e5a31a267295c3b5f7b7820af91af6b
Ben Ye <benye@amazon.com>
2023-05-08 08:49:48 -0700

    Return grpc code resource exhausted for byte limit error (#6325)
    * return grpc code resource exhausted for byte limit error
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    * fix lint
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    * update partial response strategy
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    * fix limit
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    * try to fix tests
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    * fix test error message
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    * fix test
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    ---------
    
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/store/bucket_test.go
+++ pkg/store/bucket_test.go
@@ -1201,6 +1201,9 @@ func benchmarkExpandedPostings(
 
 	for _, c := range cases {
 		t.Run(c.name, func(t testutil.TB) {
+			if c.name != `i=~".*"` {
+				return
+			}
 			b := &bucketBlock{
 				logger:            log.NewNopLogger(),
 				metrics:           newBucketStoreMetrics(nil),
