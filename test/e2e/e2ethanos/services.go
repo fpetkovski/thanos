f2ae508e1a8a2482c081f209320f375c693774e4
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2023-05-18 16:58:11 +0200

    Add dedup e2e test for Receive
    With internal and external labels support.
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- test/e2e/e2ethanos/services.go
+++ test/e2e/e2ethanos/services.go
@@ -507,6 +507,7 @@ type ReceiveBuilder struct {
 	replication         int
 	image               string
 	nativeHistograms    bool
+	labels              []string
 }
 
 func NewReceiveBuilder(e e2e.Environment, name string) *ReceiveBuilder {
@@ -534,6 +535,11 @@ func (r *ReceiveBuilder) WithExemplarsInMemStorage(maxExemplars int) *ReceiveBui
 
 func (r *ReceiveBuilder) WithIngestionEnabled() *ReceiveBuilder {
 	r.ingestion = true
+	return r
+}
+
+func (r *ReceiveBuilder) WithLabel(name, value string) *ReceiveBuilder {
+	r.labels = append(r.labels, fmt.Sprintf(`%s="%s"`, name, value))
 	return r
 }
 
@@ -581,6 +587,10 @@ func (r *ReceiveBuilder) Init() *e2emon.InstrumentedRunnable {
 		"--tsdb.path":            filepath.Join(r.InternalDir(), "data"),
 		"--log.level":            infoLogLevel,
 		"--tsdb.max-exemplars":   fmt.Sprintf("%v", r.maxExemplars),
+	}
+
+	if len(r.labels) > 0 {
+		args["--label"] = fmt.Sprintf("%s,%s", args["--label"], strings.Join(r.labels, ","))
 	}
 
 	hashring := r.hashringConfigs
@@ -646,7 +656,7 @@ func (r *ReceiveBuilder) Init() *e2emon.InstrumentedRunnable {
 
 	return e2emon.AsInstrumented(r.f.Init(wrapWithDefaults(e2e.StartOptions{
 		Image:     r.image,
-		Command:   e2e.NewCommand("receive", e2e.BuildArgs(args)...),
+		Command:   e2e.NewCommand("receive", e2e.BuildKingpinArgs(args)...),
 		Readiness: e2e.NewHTTPReadinessProbe("http", "/-/ready", 200, 200),
 	})), "http")
 }
@@ -1094,7 +1104,7 @@ const LocalPrometheusTarget = "localhost:9090"
 
 // DefaultPromConfig returns Prometheus config that sets Prometheus to:
 // * expose 2 external labels, source and replica.
-// * optionallly scrape self. This will produce up == 0 metric which we can assert on.
+// * optionally scrape self. This will produce up == 0 metric which we can assert on.
 // * optionally remote write endpoint to write into.
 func DefaultPromConfig(name string, replica int, remoteWriteEndpoint, ruleFile string, scrapeTargets ...string) string {
 	var targets string
