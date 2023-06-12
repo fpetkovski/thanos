2f37bea45131498708be91c5a1c6a0f94a50c661
Michael Hoffmann <mhoffm@posteo.de>
2023-06-12 14:10:25 +0200

    Receive: make tsdb stats limit configurable (#6437)
    * Receive: make tsdb stats limit configurable
    
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>
    
    * Receive: make tsdb stats limit configurable
    
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>
    
    ---------
    
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>

--- pkg/receive/multitsdb.go
+++ pkg/receive/multitsdb.go
@@ -44,7 +44,7 @@ import (
 type TSDBStats interface {
 	// TenantStats returns TSDB head stats for the given tenants.
 	// If no tenantIDs are provided, stats for all tenants are returned.
-	TenantStats(statsByLabelName string, tenantIDs ...string) []status.TenantStats
+	TenantStats(limit int, statsByLabelName string, tenantIDs ...string) []status.TenantStats
 }
 
 type MultiTSDB struct {
@@ -555,7 +555,7 @@ func (t *MultiTSDB) TSDBExemplars() map[string]*exemplars.TSDB {
 	return res
 }
 
-func (t *MultiTSDB) TenantStats(statsByLabelName string, tenantIDs ...string) []status.TenantStats {
+func (t *MultiTSDB) TenantStats(limit int, statsByLabelName string, tenantIDs ...string) []status.TenantStats {
 	t.mtx.RLock()
 	defer t.mtx.RUnlock()
 	if len(tenantIDs) == 0 {
@@ -582,7 +582,7 @@ func (t *MultiTSDB) TenantStats(statsByLabelName string, tenantIDs ...string) []
 			if db == nil {
 				return
 			}
-			stats := db.Head().Stats(statsByLabelName, 10)
+			stats := db.Head().Stats(statsByLabelName, limit)
 
 			mu.Lock()
 			defer mu.Unlock()
